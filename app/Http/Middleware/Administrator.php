e085e5f81801314a5b8c551bbdaefddb96555075
dinosaurfiles <dinosaurfiles@users.noreply.github.com>
2016-06-17 16:03:08 +0800

    Added Roles for Admin Dashboard

---
+++ app/Http/Middleware/Administrator.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Administrator
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $user = $request->user();
+
+        if ($user && $user->user_type == '0'){
+            return $next($request);
+        }else{
+            abort(404, 'No Way');
+        }
+    }
+}
